5bdf008140c5c49f881b084da31a560e87da4d1f
smartpraveen <37336680+smartpraveen@users.noreply.github.com>
2018-03-20 16:09:45 +0530

    Create vowels or consonant.py

---
+++ vowels or consonant.py
@@ -0,0 +1,5 @@
+s=input("enter the character:")
+if ((s=='a'or s=='A') or (s=='e'or s=='E') or (s=='i'or s=='I') or (s=='o'or s=='O') or (s=='u'or s=='U')):
+	print("Vowel")
+else:
+	print("Consonat")
